617979aa4576330c6bbf7480b6b650b4d85ccf8e
AlinaValyarova <113130561+AlinaValyarova@users.noreply.github.com>
2023-04-06 22:25:15 +0300

    добавилаа что-то

---
+++ TaskManagerApp/ForgotPassword.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace TaskManagerApp
+{
+    partial class ForgotPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginL = new System.Windows.Forms.Label();
+            this.UserEmailTB = new System.Windows.Forms.TextBox();
+            this.LoginBTN = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoginL
+            // 
+            this.LoginL.AutoSize = true;
+            this.LoginL.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginL.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.LoginL.Location = new System.Drawing.Point(92, 45);
+            this.LoginL.Name = "LoginL";
+            this.LoginL.Size = new System.Drawing.Size(219, 31);
+            this.LoginL.TabIndex = 2;
+            this.LoginL.Text = "Войти в аккаунт";
+            // 
+            // UserEmailTB
+            // 
+            this.UserEmailTB.BackColor = System.Drawing.Color.LightGray;
+            this.UserEmailTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.UserEmailTB.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.UserEmailTB.ForeColor = System.Drawing.Color.Black;
+            this.UserEmailTB.Location = new System.Drawing.Point(52, 117);
+            this.UserEmailTB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.UserEmailTB.Name = "UserEmailTB";
+            this.UserEmailTB.Size = new System.Drawing.Size(282, 22);
+            this.UserEmailTB.TabIndex = 4;
+            this.UserEmailTB.Text = "E-mail";
+            // 
+            // LoginBTN
+            // 
+            this.LoginBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoginBTN.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginBTN.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.LoginBTN.Location = new System.Drawing.Point(98, 178);
+            this.LoginBTN.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.LoginBTN.Name = "LoginBTN";
+            this.LoginBTN.Size = new System.Drawing.Size(196, 36);
+            this.LoginBTN.TabIndex = 6;
+            this.LoginBTN.Text = "Войти";
+            this.LoginBTN.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(12, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // ForgotPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(429, 249);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LoginBTN);
+            this.Controls.Add(this.UserEmailTB);
+            this.Controls.Add(this.LoginL);
+            this.Name = "ForgotPassword";
+            this.Text = "ForgotPassword";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LoginL;
+        private System.Windows.Forms.TextBox UserEmailTB;
+        private System.Windows.Forms.Button LoginBTN;
+        private System.Windows.Forms.Label label2;
+    }
+}
--- TaskManagerApp/Login.Designer.cs
+++ TaskManagerApp/Login.Designer.cs
@@ -34,6 +34,7 @@
             this.LoginBTN = new System.Windows.Forms.Button();
             this.NoAccL = new System.Windows.Forms.Label();
             this.NoAccLinkL = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // LoginL
@@ -41,9 +42,9 @@
             this.LoginL.AutoSize = true;
             this.LoginL.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.LoginL.ForeColor = System.Drawing.Color.SeaGreen;
-            this.LoginL.Location = new System.Drawing.Point(100, 104);
+            this.LoginL.Location = new System.Drawing.Point(89, 83);
             this.LoginL.Name = "LoginL";
-            this.LoginL.Size = new System.Drawing.Size(251, 36);
+            this.LoginL.Size = new System.Drawing.Size(219, 31);
             this.LoginL.TabIndex = 1;
             this.LoginL.Text = "Войти в аккаунт";
             // 
@@ -53,9 +54,10 @@
             this.UserEmailTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.UserEmailTB.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.UserEmailTB.ForeColor = System.Drawing.Color.Black;
-            this.UserEmailTB.Location = new System.Drawing.Point(70, 184);
+            this.UserEmailTB.Location = new System.Drawing.Point(62, 147);
+            this.UserEmailTB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.UserEmailTB.Name = "UserEmailTB";
-            this.UserEmailTB.Size = new System.Drawing.Size(317, 26);
+            this.UserEmailTB.Size = new System.Drawing.Size(282, 22);
             this.UserEmailTB.TabIndex = 3;
             this.UserEmailTB.Text = "E-mail";
             // 
@@ -65,9 +67,10 @@
             this.UserPasswordTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.UserPasswordTB.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.UserPasswordTB.ForeColor = System.Drawing.Color.Black;
-            this.UserPasswordTB.Location = new System.Drawing.Point(70, 233);
+            this.UserPasswordTB.Location = new System.Drawing.Point(62, 186);
+            this.UserPasswordTB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.UserPasswordTB.Name = "UserPasswordTB";
-            this.UserPasswordTB.Size = new System.Drawing.Size(317, 26);
+            this.UserPasswordTB.Size = new System.Drawing.Size(282, 22);
             this.UserPasswordTB.TabIndex = 4;
             this.UserPasswordTB.Text = "Пароль";
             // 
@@ -76,9 +79,10 @@
             this.LoginBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.LoginBTN.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.LoginBTN.ForeColor = System.Drawing.Color.SeaGreen;
-            this.LoginBTN.Location = new System.Drawing.Point(118, 289);
+            this.LoginBTN.Location = new System.Drawing.Point(105, 231);
+            this.LoginBTN.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.LoginBTN.Name = "LoginBTN";
-            this.LoginBTN.Size = new System.Drawing.Size(221, 45);
+            this.LoginBTN.Size = new System.Drawing.Size(196, 36);
             this.LoginBTN.TabIndex = 5;
             this.LoginBTN.Text = "Войти";
             this.LoginBTN.UseVisualStyleBackColor = true;
@@ -88,9 +92,9 @@
             // 
             this.NoAccL.AutoSize = true;
             this.NoAccL.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.NoAccL.Location = new System.Drawing.Point(171, 348);
+            this.NoAccL.Location = new System.Drawing.Point(152, 278);
             this.NoAccL.Name = "NoAccL";
-            this.NoAccL.Size = new System.Drawing.Size(102, 19);
+            this.NoAccL.Size = new System.Drawing.Size(88, 16);
             this.NoAccL.TabIndex = 6;
             this.NoAccL.Text = "Нет аккаунта?";
             // 
@@ -98,19 +102,31 @@
             // 
             this.NoAccLinkL.AutoSize = true;
             this.NoAccLinkL.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.NoAccLinkL.Location = new System.Drawing.Point(155, 367);
+            this.NoAccLinkL.Location = new System.Drawing.Point(138, 294);
             this.NoAccLinkL.Name = "NoAccLinkL";
-            this.NoAccLinkL.Size = new System.Drawing.Size(144, 19);
+            this.NoAccLinkL.Size = new System.Drawing.Size(124, 16);
             this.NoAccLinkL.TabIndex = 7;
             this.NoAccLinkL.TabStop = true;
             this.NoAccLinkL.Text = "Зарегистрироваться";
             this.NoAccLinkL.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.NoAccLinkL_LinkClicked);
             // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(138, 321);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(118, 17);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Забыли пароль?";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 450);
+            this.ClientSize = new System.Drawing.Size(399, 360);
+            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.NoAccLinkL);
             this.Controls.Add(this.NoAccL);
             this.Controls.Add(this.LoginBTN);
@@ -118,6 +134,7 @@
             this.Controls.Add(this.UserEmailTB);
             this.Controls.Add(this.LoginL);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -132,5 +149,6 @@
         private System.Windows.Forms.Button LoginBTN;
         private System.Windows.Forms.Label NoAccL;
         private System.Windows.Forms.LinkLabel NoAccLinkL;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
